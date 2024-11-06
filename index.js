6efd9145ad19c17649f40ad986ccc88d68851322
ShubhPundir <shb.pndr@gmail.com>
2024-11-06 11:09:34 +0530

    routing

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
+var things = require('./things.js')
 
-app.get('/', function(req,res){
-    res.send("Hello World");
-    req.body = "Hi";
+function fun(req, res){
+    return res.send("Hello From fun");
+}
+
+// app.get('/', function(req, res){
+//     res.send("Hello World");
+// });
+
+app.use('/things',things);
+
+app.get('/fun', fun);
+app.post('/fun', function(req, res){
+    res.send("You just called the post method at '/fun'!\n");
 });
+app.all('/test', function(req, res){
+    res.send("HTTP method doesn't have any effect on this route!");
+}); // USED FOR MIDDLEWAREs
 
-app.listen(3000);
+app.listen(3000, function(){
+    console.log("Server running on http://localhost:3000");
+});
